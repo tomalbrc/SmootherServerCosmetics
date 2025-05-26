95f96a22bd79c6803198d2fa53beeda1a236a7e2
tomalbrc <tomalbrc@me.com>
2025-05-26 21:14:29 +0200

    oops

--- src/main/java/de/tomalbrc/smootherservercosmetics/mixin/DisplayRendererMixin.java
+++ src/main/java/de/tomalbrc/smootherservercosmetics/mixin/DisplayRendererMixin.java
@@ -1,33 +1,26 @@
 package de.tomalbrc.smootherservercosmetics.mixin;
 
 import net.minecraft.client.renderer.entity.DisplayRenderer;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.state.DisplayEntityRenderState;
 import net.minecraft.world.entity.Display;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DisplayRenderer.class)
 public abstract class DisplayRendererMixin<T extends Display, S extends DisplayEntityRenderState> extends EntityRenderer<T, S> {
-    @Shadow @Final private EntityRenderDispatcher entityRenderDispatcher;
-
     protected DisplayRendererMixin(EntityRendererProvider.Context context) {
         super(context);
     }
 
-    @Inject(method = "extractRenderState(Lnet/minecraft/world/entity/Display;Lnet/minecraft/client/renderer/entity/state/DisplayEntityRenderState;F)V", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "extractRenderState(Lnet/minecraft/world/entity/Display;Lnet/minecraft/client/renderer/entity/state/DisplayEntityRenderState;F)V", at = @At("RETURN"))
     private void applyPlayerVehicleRot(T display, S displayEntityRenderState, float f, CallbackInfo ci) {
-        if (display.isPassenger() && display instanceof Display.ItemDisplay) {
-            var vehicle = display.getVehicle();
-            if (vehicle != null) {
-                displayEntityRenderState.entityYRot = vehicle.getYRot();
-            }
+        if (display.isPassenger() && display instanceof Display.ItemDisplay && display.getVehicle() instanceof Player player) {
+            displayEntityRenderState.entityYRot = player.getPreciseBodyRotation(f);
         }
     }
 }
--- src/main/java/de/tomalbrc/smootherservercosmetics/mixin/EntityRendererMixin.java
+++ src/main/java/de/tomalbrc/smootherservercosmetics/mixin/EntityRendererMixin.java
@@ -2,32 +2,24 @@ package de.tomalbrc.smootherservercosmetics.mixin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.culling.Frustum;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.world.entity.Display;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityRenderer.class)
 public abstract class EntityRendererMixin<T extends Entity, S extends EntityRenderState> {
-    @Shadow @Final
-    protected EntityRenderDispatcher entityRenderDispatcher;
-
     @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
     public void shouldRender(T entity, Frustum frustum, double d, double e, double f, CallbackInfoReturnable<Boolean> cir) {
         if (entity.isPassenger() && entity instanceof Display.ItemDisplay && entity.getVehicle() instanceof Player player) {
             if (player == Minecraft.getInstance().player && Minecraft.getInstance().options.getCameraType().isFirstPerson()) {
-                cir.cancel();
+                cir.setReturnValue(false);
             }
-
-            cir.setReturnValue(false);
         }
     }
 }
